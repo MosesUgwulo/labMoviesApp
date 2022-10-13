e55b87d557d142e48f83bf5f0875eda7431791a9
Moses Ugwulo <74902008+MosesUgwulo@users.noreply.github.com>
2022-10-13 11:24:51 +0100

    Exercise - Show favourite icon on Movie Details page..

--- moviesApp/src/components/headerMovie/index.js
+++ moviesApp/src/components/headerMovie/index.js
@@ -5,9 +5,12 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
+import Avatar from "@mui/material/Avatar";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const MovieHeader = (props) => {
   const movie = props.movie;
+  const favouriteMovies = JSON.parse(localStorage.getItem("favourites")); 
 
   return (
     <Paper 
@@ -23,6 +26,15 @@ const MovieHeader = (props) => {
       <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
+        
+      {/* display the heart with an embedded FavoriteIcon or null depending on some condition test. Use the ternary operator to express this, i.e. condition test ? true case : false case */}
+
+      {favouriteMovies && favouriteMovies.find(m => m.id === movie.id) ? (
+        <Avatar sx={{ backgroundColor: 'red' }}>
+          <FavoriteIcon />
+        </Avatar>
+      ) : null}
+
 
       <Typography variant="h4" component="h3">
         {movie.title}
